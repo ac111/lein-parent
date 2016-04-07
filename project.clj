72abd1d8778fed873c360e752ebde713b0fd6a37
Andrew Ceponkus <andrew.ceponkus@cenx.com>
2016-04-07 10:18:56 -0400

    uprev

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-parent "0.2.2-SNAPSHOT"
+(defproject lein-parent "0.2.3-SNAPSHOT"
   :description "Leiningen plugin for inheriting properties from a parent project"
   :url "https://github.com/achin/lein-parent"
   :license {:name "Eclipse Public License"
